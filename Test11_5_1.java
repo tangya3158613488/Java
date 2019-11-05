4384260b54cee6e76ae922f5b53471d45a5a3ff4
tangya3158613488 <3158613488@qq.com>
2019-11-05 22:15:29 +0800

    提交代码

---
+++ Test11_5_1.java
@@ -0,0 +1,10 @@
+package P3;
+
+import java.io.*;
+public class Test11_5_1{
+    public static void main(String[] args) throws IOException{
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+         System.out.println("Please insert a integer");
+         int a = Integer.parseInt(br.readLine()); 
+        System.out.println("You input :"+a);
+}}

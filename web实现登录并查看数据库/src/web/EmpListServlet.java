b6dff6a609251b9ccae18af94f6465a8ab6d8d19
tangya3158613488 <3158613488@qq.com>
2019-11-27 22:32:59 +0800

    提交代码

---
+++ web实现登录并查看数据库/src/web/EmpListServlet.java
@@ -0,0 +1,31 @@
+package web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.EmpDao;
+import entity.Emp;
+
+public class EmpListServlet extends HttpServlet{
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		EmpDao dao=new EmpDao();
+		String tmp=request.getParameter("current");
+		int current;
+		if(tmp==null){
+			current=1;
+		}else{
+			current=Integer.parseInt(tmp);
+		}
+		ArrayList<Emp> list=dao.selectAll(current,20);
+		request.setAttribute("emps", list);
+		request.getRequestDispatcher("listEmp.jsp").forward(request, response);
+		
+	}
+}

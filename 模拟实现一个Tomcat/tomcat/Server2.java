1c2771ff3af1e6acf4c5843c2c96d1a2668b0e93
tangya3158613488 <3158613488@qq.com>
2019-12-01 17:57:10 +0800

    提交代码

---
+++ 模拟实现一个Tomcat/tomcat/Server2.java
@@ -0,0 +1,68 @@
+package Exercise;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import javax.activation.UnsupportedDataTypeException;
+public class Server2 {
+	ServerSocket ser;
+	public Server2()
+	{
+		try{
+			ser = new ServerSocket(8080);
+			System.out.println("tomcat开始启动");
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	public void start()
+	{
+		try {
+			Socket client = ser.accept();
+			InputStream is = client.getInputStream();
+			InputStreamReader isr = new InputStreamReader(is,"ISO-8859-1");
+			BufferedReader br = new BufferedReader(isr);
+			String str = "";
+			while(!(str = br.readLine()).equals("")){
+				System.out.println(str);
+			}
+			OutputStream os = client.getOutputStream();
+			File file = new File("webapps/index.html");
+			os.write("HTTP/1.1 200 OK".getBytes("iso-8859-1"));
+			os.write(13);
+			os.write(10);
+			//告诉浏览器响应里的
+			os.write("Content-Type: text/html;charset=utf-8".getBytes("iso-8859-1"));
+			os.write(13);
+			os.write(10);
+			os.write(("Content-Length: "+file.length()).getBytes("iso-8859-1"));
+			os.write(13);
+			os.write(10);
+			os.write("".getBytes("iso-8859-1"));
+			os.write(13);
+			os.write(10);
+			FileInputStream fis = new FileInputStream(file);
+			byte[] b = new byte[(int)file.length()];
+			fis.read(b);
+			fis.close();
+			os.write(b);
+			os.flush();
+			os.close();
+		} catch (UnsupportedDataTypeException e) {
+			e.printStackTrace();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args){
+		Server2 s = new Server2();
+		s.start();
+	}
+}
